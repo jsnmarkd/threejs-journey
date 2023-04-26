627650e29b27620f7e1cf8fb901105dfd59ad555
Jason Divinagracia <jmarkd04@gmail.com>
2023-04-26 18:11:19 -0400

    Implement Orbit Control

--- 01-basics/07-cameras/src/script.js
+++ 01-basics/07-cameras/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 /**
  * Cursor
@@ -61,6 +62,14 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 1;
+// controls.update();
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -79,10 +88,14 @@ const tick = () => {
   // Update Camera
   // camera.position.x = cursor.x * 10;
   // camera.position.y = cursor.y * 10;
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-  camera.lookAt(mesh.position);
+
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
+
+  // Update Controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
