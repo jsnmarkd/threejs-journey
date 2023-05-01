b1ed5494eb1a5bf7dd72f800de47e52b7b058f9d
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-01 18:49:27 -0400

    Refactor code

--- 01-basics/13-3d-text/src/script.js
+++ 01-basics/13-3d-text/src/script.js
@@ -51,19 +51,17 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   // );
   textGeometry.center();
 
-  const textMaterial = new THREE.MeshMatcapMaterial();
-  textMaterial.matcap = matcapTexture;
-  // textMaterial.wireframe = true;
-  const text = new THREE.Mesh(textGeometry, textMaterial);
+  const material = new THREE.MeshMatcapMaterial();
+  material.matcap = matcapTexture;
+  // material.wireframe = true;
+  const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
 
   console.time('donuts');
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
 
-  for (let i = 0; i < 100; i++) {
-    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
-    const donutMaterial = new THREE.MeshMatcapMaterial();
-    donutMaterial.matcap = matcapTexture;
-    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+  for (let i = 0; i < 300; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
 
     donut.position.x = (Math.random() - 0.5) * 10;
     donut.position.y = (Math.random() - 0.5) * 10;
