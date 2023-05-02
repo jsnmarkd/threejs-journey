d48cf49b2febe83d35c02b4d266ee6dbd29b4323
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-02 16:07:07 -0400

    Use baked shadow alternative and animate

--- 02-classic-techniques/16-shadows/src/script.js
+++ 02-classic-techniques/16-shadows/src/script.js
@@ -7,6 +7,7 @@ import * as dat from "lil-gui";
  */
 const textureLoader = new THREE.TextureLoader();
 const bakedShadow = textureLoader.load("textures/bakedShadow.jpg");
+const simpleShadow = textureLoader.load("textures/simpleShadow.jpg");
 
 /**
  * Base
@@ -103,13 +104,26 @@ sphere.castShadow = true;
 
 const plane = new THREE.Mesh(
   new THREE.PlaneGeometry(5, 5),
-  new THREE.MeshBasicMaterial({ map: bakedShadow })
+  // new THREE.MeshBasicMaterial({ map: bakedShadow })
+  material
 );
 plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = -0.5;
 plane.receiveShadow = true;
 
 scene.add(sphere, plane);
+
+const sphereShadow = new THREE.Mesh(
+  new THREE.PlaneGeometry(1.5, 1.5),
+  new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    transparent: true,
+    alphaMap: simpleShadow,
+  })
+);
+sphereShadow.rotation.x = -Math.PI * 0.5;
+sphereShadow.position.y = plane.position.y + 0.01;
+scene.add(sphereShadow);
 
 /**
  * Sizes
@@ -171,6 +185,16 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update the Sphere
+  sphere.position.x = Math.cos(elapsedTime) * 1.5;
+  sphere.position.z = Math.sin(elapsedTime) * 1.5;
+  sphere.position.y = Math.abs(Math.sin(elapsedTime * 3));
+
+  // Update Shadow
+  sphereShadow.position.x = sphere.position.x;
+  sphereShadow.position.z = sphere.position.z;
+  sphereShadow.material.opacity = (1 - sphere.position.y) * 0.5;
 
   // Update controls
   controls.update();
