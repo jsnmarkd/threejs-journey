12f9a5bb9893ff9483227cd794f0dfdb8e045fe4
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-02 15:17:15 -0400

    Add shadow

--- 02-classic-techniques/16-shadows/src/script.js
+++ 02-classic-techniques/16-shadows/src/script.js
@@ -31,6 +31,8 @@ gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001);
 scene.add(directionalLight);
 
+directionalLight.castShadow = true;
+
 /**
  * Materials
  */
@@ -43,10 +45,12 @@ gui.add(material, "roughness").min(0).max(1).step(0.001);
  * Objects
  */
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
+sphere.castShadow = true;
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = -0.5;
+plane.receiveShadow = true;
 
 scene.add(sphere, plane);
 
@@ -99,6 +103,8 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+renderer.shadowMap.enabled = true;
 
 /**
  * Animate
