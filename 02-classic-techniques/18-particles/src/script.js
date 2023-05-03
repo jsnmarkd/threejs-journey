b2b97cae6bcde2fd0778ff5eec89342a6100be4e
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-03 18:32:20 -0400

    Implement custom particles

--- 02-classic-techniques/18-particles/src/script.js
+++ 02-classic-techniques/18-particles/src/script.js
@@ -24,16 +24,29 @@ const textureLoader = new THREE.TextureLoader();
  */
 
 // Geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+const particlesGeometry = new THREE.BufferGeometry(1, 32, 32);
+const count = 5000;
+
+const positions = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10;
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-  size: 0.02,
+  size: 0.1,
   sizeAttenuation: true,
 });
 
-// Points 
-const particles = new THREE.Points(particlesGeometry,particlesMaterial)
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 scene.add(particles);
 
 /**
