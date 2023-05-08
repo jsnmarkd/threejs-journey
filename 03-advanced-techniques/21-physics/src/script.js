bba5f631894dc1da7d26b651f3549c95c977c1ae
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-08 15:05:18 -0400

    Implement contact material

--- 03-advanced-techniques/21-physics/src/script.js
+++ 03-advanced-techniques/21-physics/src/script.js
@@ -40,18 +40,33 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
+// Materials
+// const concreteMaterial = new CANNON.Material("concrete");
+// const plasticMaterial = new CANNON.Material("plastic");
+const defaultMaterial = new CANNON.Material("default");
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  { friction: 0.1, restitution: 0.7 }
+);
+world.addContactMaterial(defaultContactMaterial);
+// world.defaultContactMaterial = defaultContactMaterial; 
+
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
+  material: defaultMaterial
 });
 world.add(sphereBody);
 
 // Floor
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
+floorBody.material = defaultMaterial;
 floorBody.mass = 0;
 floorBody.addShape(floorShape);
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
