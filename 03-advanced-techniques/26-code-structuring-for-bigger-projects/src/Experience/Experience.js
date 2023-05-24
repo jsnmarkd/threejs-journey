2ad31a5fcdee9e02bcff8b2c5388924a06b40387
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-24 15:40:00 -0400

    Create Camera Class with singleton

--- 03-advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 03-advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,9 +1,18 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import Sizes from "./Utils/Sizes";
 import Time from "./Utils/Time";
+import Camera from "./Camera";
+
+let instance = null;
 
 export default class Experience {
   constructor(canvas) {
+    // Singleton
+    if (instance) {
+      return instance;
+    }
+    instance = this;
+
     // Global access
     window.experience = this;
 
@@ -13,7 +22,8 @@ export default class Experience {
     // Setup
     this.sizes = new Sizes();
     this.time = new Time();
-    this.scene = new THREE.Scene()
+    this.scene = new THREE.Scene();
+    this.camera = new Camera();
 
     // Sizes resize Event
     this.sizes.on("resize", () => {
@@ -25,6 +35,10 @@ export default class Experience {
       this.update();
     });
   }
-  resize() {}
-  update() {}
+  resize() {
+    this.camera.resize();
+  }
+  update() {
+    this.camera.update();
+  }
 }
