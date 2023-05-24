05fca7f149fc2fda62c5079652cf4c270b6ee247
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-24 15:02:33 -0400

    Create Utils folder and create Sizes & EventEmitter Class

--- 03-advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 03-advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,3 +1,5 @@
+import Sizes from "./Utils/Sizes";
+
 export default class Experience {
   constructor(canvas) {
     // Global access
@@ -6,5 +8,7 @@ export default class Experience {
     // Options
     this.canvas = canvas;
 
+    // Setup
+    this.sizes = new Sizes();
   }
 }
