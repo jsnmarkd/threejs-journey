86af1522e018f1c08296618738625536908ec0f8
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-25 16:17:57 -0400

    Create Floor class and remove test cube

--- 03-advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 03-advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import Experience from "../Experience";
 import Environment from "./Environment";
+import Floor from "./Floor";
 
 export default class World {
   constructor() {
@@ -8,17 +9,11 @@ export default class World {
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
 
-    // Test Mesh
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial()
-    );
-    this.scene.add(testMesh);
-
     // Wait for resources
     this.resources.on('ready', () => {
       // Setup
       this.environment = new Environment();
+      this.floor = new Floor();
     })
   }
 }
