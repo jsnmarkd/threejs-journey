3d25c959a1a80df043c1f93a935be2d2954f5a37
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-30 09:06:55 -0400

    Implement Varyings

--- 04-shaders/27-shaders/src/shaders/test/vertex.glsl
+++ 04-shaders/27-shaders/src/shaders/test/vertex.glsl
@@ -5,6 +5,8 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 attribute float aRandom;
 
+varying float vRandom;
+
 void main() {
 
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -15,6 +17,8 @@ void main() {
   vec4 projectionPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectionPosition;
+
+  vRandom = aRandom;
 
   // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
