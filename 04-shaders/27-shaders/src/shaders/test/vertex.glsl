c825c537996f594f47122d00c62f42e0f04af380
Jason Divinagracia <jmarkd04@gmail.com>
2023-05-30 10:40:50 -0400

    Add color variation

--- 04-shaders/27-shaders/src/shaders/test/vertex.glsl
+++ 04-shaders/27-shaders/src/shaders/test/vertex.glsl
@@ -9,13 +9,17 @@ attribute vec2 uv;
 // attribute float aRandom;
 
 varying vec2 vUv;
+varying float vElevation;
 // varying float vRandom;
 
 void main() {
 
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-  modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+  modelPosition.z += elevation;
   // modelPosition.z += aRandom * 0.1;
 
   vec4 viewPosition = viewMatrix * modelPosition;
@@ -24,6 +28,8 @@ void main() {
   gl_Position = projectionPosition;
 
   vUv = uv;
+
+  vElevation = elevation;
 
   // vRandom = aRandom;
 
