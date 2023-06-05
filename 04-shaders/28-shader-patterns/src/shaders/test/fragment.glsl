4c7b2747c816c429487bbd1389490e84ed669380
Jason Divinagracia <jmarkd04@gmail.com>
2023-06-05 10:42:01 -0400

    Pattern 2, 3 - lesson 28

--- 04-shaders/28-shader-patterns/src/shaders/test/fragment.glsl
+++ 04-shaders/28-shader-patterns/src/shaders/test/fragment.glsl
@@ -2,5 +2,7 @@ varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(vUv, 1.0, 1.0);
+    // gl_FragColor = vec4(vUv, 1.0, 1.0); // pattern 1
+    // gl_FragColor = vec4(vUv, 0.0, 1.0); // pattern 2
+    gl_FragColor = vec4(vUv, 0.5, 1.0); // pattern 3
 }
