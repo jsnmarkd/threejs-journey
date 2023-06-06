73d0e64f107d2991b3aa9b23de1786c89033ea32
Jason Divinagracia <jmarkd04@gmail.com>
2023-06-06 15:15:08 -0400

    Pattern 9, 10, 11 - lesson 28

--- 04-shaders/28-shader-patterns/src/shaders/test/fragment.glsl
+++ 04-shaders/28-shader-patterns/src/shaders/test/fragment.glsl
@@ -3,6 +3,7 @@ varying vec2 vUv;
 void main()
 {
     // gl_FragColor = vec4(vUv, 1.0, 1.0); // pattern 1
+
     // gl_FragColor = vec4(vUv, 0.0, 1.0); // pattern 2.a
     // gl_FragColor = vec4(vUv, 0.5, 1.0); // pattern 2.b
 
@@ -21,7 +22,19 @@ void main()
     // float strength = mod(vUv.y * 10.0, 1.0); // pattern 7
     // gl_FragColor = vec4(strength, strength, strength, 1.0); 
 
-    float strength = mod(vUv.y * 10.0, 1.0); // pattern 8
-    strength = step(0.5, strength);
+    // float strength = mod(vUv.y * 10.0, 1.0); // pattern 8
+    // strength = step(0.5, strength);
+    // gl_FragColor = vec4(strength, strength, strength, 1.0); 
+
+    // float strength = mod(vUv.y * 10.0, 1.0); // pattern 9
+    // strength = step(0.8, strength);
+    // gl_FragColor = vec4(strength, strength, strength, 1.0); 
+    
+    // float strength = mod(vUv.x * 10.0, 1.0); // pattern 10
+    // strength = step(0.8, strength);
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
+
+    float strength = step(0.8, mod(vUv.x * 10.0, 1.0)); // pattern 11
+    strength += step(0.8, mod(vUv.y * 10.0, 1.0));
     gl_FragColor = vec4(strength, strength, strength, 1.0); 
 }
