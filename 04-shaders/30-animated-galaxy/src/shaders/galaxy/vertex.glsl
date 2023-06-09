153037e771d4c0372ea09cf8365e0b219de7c35d
Jason Divinagracia <jmarkd04@gmail.com>
2023-06-09 13:29:14 -0400

    Handle the size of the particles

--- 04-shaders/30-animated-galaxy/src/shaders/galaxy/vertex.glsl
+++ 04-shaders/30-animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -1,3 +1,7 @@
+uniform float uSize;
+
+attribute float aScale;
+
 void main() 
 {
   /**
@@ -11,5 +15,6 @@ void main()
   /**
    * Size
    */
-  gl_PointSize = 1.0;
+  gl_PointSize = uSize * aScale;
+  gl_PointSize *= (1.0 / - viewPosition.z);
 }
