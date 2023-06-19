b360e7a4ebe6516c7fafe428292d4c9d48214886
Jason Divinagracia <jmarkd04@gmail.com>
2023-06-19 15:34:12 -0400

    Implement Tips 1 to 18, add comments

--- 05-extra/33-performance-tips/src/script.js
+++ 05-extra/33-performance-tips/src/script.js
@@ -155,58 +155,59 @@ tick();
  * Tips
  */
 
-// // Tip 4
+// // Tip 4 - Renderer Information
 // console.log(renderer.info)
 
-// // Tip 6
+// // Tip 6 - How to Dispose
 // scene.remove(cube)
 // cube.geometry.dispose()
 // cube.material.dispose()
 
-// // Tip 10
-// directionalLight.shadow.camera.top = 3
-// directionalLight.shadow.camera.right = 6
-// directionalLight.shadow.camera.left = - 6
-// directionalLight.shadow.camera.bottom = - 3
-// directionalLight.shadow.camera.far = 10
-// directionalLight.shadow.mapSize.set(1024, 1024)
+// // Tip 10 - Optimize Shadow Maps
+directionalLight.shadow.camera.top = 3
+directionalLight.shadow.camera.right = 6
+directionalLight.shadow.camera.left = - 6
+directionalLight.shadow.camera.bottom = - 3
+directionalLight.shadow.camera.far = 10
+directionalLight.shadow.mapSize.set(1024, 1024)
 
-// const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
-// scene.add(cameraHelper)
+const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(cameraHelper)
 
-// // Tip 11
-// cube.castShadow = true
-// cube.receiveShadow = false
+// // Tip 11 - Use castShadow and receiveShadow wisely
+cube.castShadow = true
+cube.receiveShadow = false
 
-// torusKnot.castShadow = true
-// torusKnot.receiveShadow = false
+torusKnot.castShadow = true
+torusKnot.receiveShadow = false
 
-// sphere.castShadow = true
-// sphere.receiveShadow = false
+sphere.castShadow = true
+sphere.receiveShadow = false
 
-// floor.castShadow = false
-// floor.receiveShadow = true
+floor.castShadow = false
+floor.receiveShadow = true
 
-// // Tip 12
-// renderer.shadowMap.autoUpdate = false
-// renderer.shadowMap.needsUpdate = true
+// // Tip 12 - Deactivate shadow auto update
+renderer.shadowMap.autoUpdate = false
+renderer.shadowMap.needsUpdate = true
 
-// // Tip 18
-// for(let i = 0; i < 50; i++)
-// {
-//     const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+// // Tip 18 - Mutualize Geometries
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
 
-//     const material = new THREE.MeshNormalMaterial()
+for(let i = 0; i < 50; i++)
+{
 
-//     const mesh = new THREE.Mesh(geometry, material)
-//     mesh.position.x = (Math.random() - 0.5) * 10
-//     mesh.position.y = (Math.random() - 0.5) * 10
-//     mesh.position.z = (Math.random() - 0.5) * 10
-//     mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
-//     mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+    const material = new THREE.MeshNormalMaterial()
 
-//     scene.add(mesh)
-// }
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = (Math.random() - 0.5) * 10
+    mesh.position.y = (Math.random() - 0.5) * 10
+    mesh.position.z = (Math.random() - 0.5) * 10
+    mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+    mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+
+    scene.add(mesh)
+}
 
 // // Tip 19
 // for(let i = 0; i < 50; i++)
