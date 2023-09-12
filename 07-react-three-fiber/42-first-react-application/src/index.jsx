ce5f8378ffa4a480f642621c024c56b6c96c685e
Jason Divinagracia <jmarkd04@gmail.com>
2023-09-12 15:00:40 -0400

    Add in-line styling

--- 07-react-three-fiber/42-first-react-application/src/index.jsx
+++ 07-react-three-fiber/42-first-react-application/src/index.jsx
@@ -3,16 +3,16 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.querySelector("#root"));
 
-const toto = false;
+const toto = "there";
 
 root.render(
   <>
-    <h1 className="title">
-      {/* This is a comment */}
-      Hello {toto ? "yes" : "no"}
+    <h1 className="title" style={ { color: "coral", backgroundColor: 'floralwhite' } }>
+      {/* Some comment */}
+      Hello {toto}
     </h1>
     <p>
-      Some <br /> content
+      Some content <br /> {Math.random()}
     </p>
     <input type="checkbox" id="the-checkbox" />
     <label htmlFor="the-checkbox">Check me!</label>
