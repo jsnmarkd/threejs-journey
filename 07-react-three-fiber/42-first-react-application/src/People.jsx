208b127542dfe08cc889fa0571cf980c97fd8339
Jason Divinagracia <jmarkd04@gmail.com>
2023-09-18 14:29:34 -0400

    React Review - Fetching data from an API

--- 07-react-three-fiber/42-first-react-application/src/People.jsx
+++ 07-react-three-fiber/42-first-react-application/src/People.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function People() {
   const [people, setPeople] = useState([
@@ -7,6 +7,21 @@ export default function People() {
     { id: 3, name: "Riley" },
     { id: 4, name: "Chachi" },
   ]);
+
+  const getPeople = async () => {
+
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then((response) => response.json())
+    //   .then((result) => setPeople(result));
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const result = await response.json();
+    setPeople(result);
+  };
+
+  useEffect(() => {
+    getPeople();
+  }, []);
 
   return (
     <div>
