d3327bb748a15c1714d4144b47a374741e099d89
Jason Divinagracia <jmarkd04@gmail.com>
2023-09-20 16:20:17 -0400

    Practice - Add more meshes

--- 07-react-three-fiber/43-first-r3f-application/src/Experience.jsx
+++ 07-react-three-fiber/43-first-r3f-application/src/Experience.jsx
@@ -1,9 +1,17 @@
 export default function Experience() {
   return (
     <>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshBasicMaterial color="orange" />
+      </mesh>
       <mesh rotation-y={Math.PI * 0.23} position-x={2} scale={1.5}>
         <boxGeometry scale={1.5} />
-        <meshBasicMaterial color="mediumpurple" wireframe={true} />
+        <meshBasicMaterial color="mediumpurple" />
+      </mesh>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" />
       </mesh>
     </>
   );
