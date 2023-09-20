c00fcf1cee8b9ba52f2f9ae56410525b7dee424f
Jason Divinagracia <jmarkd04@gmail.com>
2023-09-20 18:13:53 -0400

    Setup some Canvas settings - Camera

--- 07-react-three-fiber/43-first-r3f-application/src/index.jsx
+++ 07-react-three-fiber/43-first-r3f-application/src/index.jsx
@@ -5,8 +5,15 @@ import Experience from "./Experience.jsx";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
+const cameraSettings = {
+  fov: 45,
+  near: 0.1,
+  far: 200,
+  position: [3, 2, 6],
+};
+
 root.render(
-  <Canvas>
+  <Canvas camera={cameraSettings}>
     <Experience />
   </Canvas>
 );
