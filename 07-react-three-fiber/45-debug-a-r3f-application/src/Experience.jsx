6f8b112e7dbd25bcb52acac0a15dd21f5b3aef15
Jason Divinagracia <jmarkd04@gmail.com>
2023-09-28 16:12:59 -0400

    Implement Leva features - Color, Boolean, Interval, Button, Select, Folders, and some configurations

--- 07-react-three-fiber/45-debug-a-r3f-application/src/Experience.jsx
+++ 07-react-three-fiber/45-debug-a-r3f-application/src/Experience.jsx
@@ -1,10 +1,21 @@
 import Cube from "./Cube.jsx";
 import { OrbitControls } from "@react-three/drei";
-import { useControls } from "leva";
+import { useControls, button } from "leva";
 
 export default function Experience() {
-  const { position } = useControls({
-    position: { value: { x: -2, y: 0 }, step: 0.01, joystick: 'invertY' },
+  const { position, color, visible, myInterval } = useControls("sphere", {
+    position: { value: { x: -2, y: 0 }, step: 0.01, joystick: "invertY" },
+    color: "#ff0000",
+    visible: true,
+    myInterval: { min: 0, max: 10, value: [4, 5] },
+    clickMe: button(() => {
+      console.log("clicked");
+    }),
+    choice: { options: ["a", "b", "c"] },
+  });
+
+  const { scale } = useControls("cube", {
+    scale: { value: 1.5, step: 0.01, min: 0, max: 5 },
   });
 
   return (
@@ -14,12 +25,12 @@ export default function Experience() {
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh position={[position.x, position.y, 0]}>
+      <mesh position={[position.x, position.y, 0]} visible={visible}>
         <sphereGeometry />
-        <meshStandardMaterial color="orange" />
+        <meshStandardMaterial color={color} />
       </mesh>
 
-      <Cube scale={2} />
+      <Cube scale={scale} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
