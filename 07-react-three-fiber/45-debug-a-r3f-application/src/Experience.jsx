693ddfe269e3066412e606dc6e1f38a66347a2b7
Jason Divinagracia <jmarkd04@gmail.com>
2023-09-28 15:52:00 -0400

    Add a Debug UI with Leva - Add vector feature to the range position

--- 07-react-three-fiber/45-debug-a-r3f-application/src/Experience.jsx
+++ 07-react-three-fiber/45-debug-a-r3f-application/src/Experience.jsx
@@ -4,7 +4,7 @@ import { useControls } from "leva";
 
 export default function Experience() {
   const { position } = useControls({
-    position: { value: -2, min: -4, max: 4, step: 0.01 },
+    position: { value: { x: -2, y: 0 }, step: 0.01, joystick: 'invertY' },
   });
 
   return (
@@ -14,7 +14,7 @@ export default function Experience() {
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh position-x={position}>
+      <mesh position={[position.x, position.y, 0]}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
