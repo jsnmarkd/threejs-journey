48b282b91af34dea99621f34a283f115f2ceb5da
Jason Divinagracia <jmarkd04@gmail.com>
2023-09-28 14:41:36 -0400

    Create Cube component, import it to the Experience component, and use the browser extension tool React Developer Tools

--- 07-react-three-fiber/45-debug-a-r3f-application/src/Experience.jsx
+++ 07-react-three-fiber/45-debug-a-r3f-application/src/Experience.jsx
@@ -1,4 +1,5 @@
 import { OrbitControls } from "@react-three/drei";
+import Cube from "./Cube.jsx";
 
 export default function Experience() {
   return (
@@ -13,10 +14,7 @@ export default function Experience() {
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
+      <Cube scale={2} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
