5fc34e709b32bf6f51039445ead16a9ac0254433
Jason Divinagracia <jmarkd04@gmail.com>
2023-09-29 20:27:12 -0400

    Add directional light

--- 07-react-three-fiber/46-environment-and-staging-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/46-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,9 +1,13 @@
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useHelper } from "@react-three/drei";
 import { useRef } from "react";
 import { Perf } from "r3f-perf";
+import * as THREE from 'three';
 
 export default function Experience() {
+  const directionalLight = useRef();
+  useHelper(directionalLight, THREE.DirectionalLightHelper, 1)
+
   const cube = useRef();
 
   useFrame((state, delta) => {
@@ -17,7 +21,7 @@ export default function Experience() {
 
       <OrbitControls makeDefault />
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight ref={directionalLight} position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
       <mesh position-x={-2}>
