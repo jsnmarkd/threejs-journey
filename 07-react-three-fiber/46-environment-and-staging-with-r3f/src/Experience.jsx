00f4c2496e293fd9dc6d5e58c3b7486b8d661a9a
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-02 19:20:11 -0400

    Implement Accumulative Shadows

--- 07-react-three-fiber/46-environment-and-staging-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/46-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,9 +1,11 @@
 import { useFrame } from "@react-three/fiber";
 import {
   OrbitControls,
-  useHelper,
+  // useHelper,
   // BakeShadows,
-  SoftShadows,
+  // SoftShadows,
+  AccumulativeShadows,
+  RandomizedLight,
 } from "@react-three/drei";
 import { useRef } from "react";
 import { Perf } from "r3f-perf";
@@ -11,29 +13,50 @@ import * as THREE from "three";
 
 export default function Experience() {
   const directionalLight = useRef();
-  useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
+  // useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
 
   const cube = useRef();
 
   useFrame((state, delta) => {
+    // const time = state.clock.getElapsedTime();
+    // cube.current.position.x = 2 + Math.sin(time);
     cube.current.rotation.y += delta * 0.2;
   });
 
   return (
     <>
       {/* <BakeShadows /> */}
-      <SoftShadows
+      {/* <SoftShadows
         frustum={3.75}
         size={50}
         near={9.5}
         samples={17}
         rings={11}
-      />
+      /> */}
 
       <color args={["ivory"]} attach={"background"} />
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
+
+      <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        color="#316d39"
+        opacity={0.8}
+        frames={Infinity}
+        temporal
+        blend={100}
+      >
+        <RandomizedLight
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={1}
+          position={[1, 2, 3]}
+          bias={0.01}
+        />
+      </AccumulativeShadows>
 
       <directionalLight
         ref={directionalLight}
@@ -60,12 +83,7 @@ export default function Experience() {
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
-      <mesh
-        position-y={-1}
-        receiveShadow
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-      >
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
