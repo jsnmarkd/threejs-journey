16ccc4023730d6eecc85621a99a515611c399cbf
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-02 11:55:04 -0400

    Add shadows - default threejs way

--- 07-react-three-fiber/46-environment-and-staging-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/46-environment-and-staging-with-r3f/src/Experience.jsx
@@ -21,20 +21,20 @@ export default function Experience() {
 
       <OrbitControls makeDefault />
 
-      <directionalLight ref={directionalLight} position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight ref={directionalLight} castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh position-x={-2}>
+      <mesh castShadow position-x={-2}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh ref={cube} position-x={2} scale={1.5}>
+      <mesh ref={cube} castShadow position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh position-y={-1} receiveShadow rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
