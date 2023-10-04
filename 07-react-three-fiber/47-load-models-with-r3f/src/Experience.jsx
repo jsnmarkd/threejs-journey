c68a0e20d6ebe0595d59cdda19363dac7d19545f
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-04 18:33:18 -0400

    Add a fallback placeholder to the suspense

--- 07-react-three-fiber/47-load-models-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/47-load-models-with-r3f/src/Experience.jsx
@@ -2,6 +2,7 @@ import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { Suspense } from "react";
 import Model from "./Model";
+import Placeholder from "./Placeholder";
 
 export default function Experience() {
   return (
@@ -23,7 +24,7 @@ export default function Experience() {
         <meshStandardMaterial color="greenyellow" />
       </mesh>
 
-      <Suspense>
+      <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]} />}>
         <Model />
       </Suspense>
     </>
