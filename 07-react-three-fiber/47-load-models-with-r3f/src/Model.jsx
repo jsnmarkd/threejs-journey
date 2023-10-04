aeed69f93c483b1dcf5ee9d5076b332b230c1b56
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-04 18:54:40 -0400

    Implement preload

--- 07-react-three-fiber/47-load-models-with-r3f/src/Model.jsx
+++ 07-react-three-fiber/47-load-models-with-r3f/src/Model.jsx
@@ -4,3 +4,5 @@ export default function Model() {
   const model = useGLTF("./hamburger-draco.glb");
   return <primitive object={model.scene} scale={0.35} />;
 }
+
+useGLTF.preload("./hamburger-draco.glb");
