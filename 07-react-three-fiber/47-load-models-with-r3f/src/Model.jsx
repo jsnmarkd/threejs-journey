db6447c7cfc9f9e2dae630b87125f0d605d8b782
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-04 18:51:11 -0400

    Use GLTF loader hook from Drei

--- 07-react-three-fiber/47-load-models-with-r3f/src/Model.jsx
+++ 07-react-three-fiber/47-load-models-with-r3f/src/Model.jsx
@@ -1,16 +1,6 @@
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { useGLTF } from "@react-three/drei";
 
 export default function Model() {
-  const model = useLoader(
-    GLTFLoader,
-    "./FlightHelmet/glTF/FlightHelmet.gltf",
-    (loader) => {
-      const dracoLoader = new DRACOLoader();
-      dracoLoader.setDecoderPath("./draco/");
-      loader.setDRACOLoader(dracoLoader);
-    }
-  );
-  return <primitive object={model.scene} scale={5} position-y={-1} />;
+  const model = useGLTF("./hamburger-draco.glb");
+  return <primitive object={model.scene} scale={0.35} />;
 }
