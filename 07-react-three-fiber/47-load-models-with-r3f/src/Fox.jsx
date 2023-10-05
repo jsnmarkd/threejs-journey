da498fa017a0e92d54e1eafd5e9cbd32405cc56a
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-04 20:31:28 -0400

    Animation control and cleanup phase

--- 07-react-three-fiber/47-load-models-with-r3f/src/Fox.jsx
+++ 07-react-three-fiber/47-load-models-with-r3f/src/Fox.jsx
@@ -1,15 +1,21 @@
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { useEffect } from "react";
+import { useControls } from "leva";
 
 export default function Fox() {
   const fox = useGLTF("./Fox/glTF/Fox.gltf");
   const animations = useAnimations(fox.animations, fox.scene);
-  console.log(animations);
+
+  const { animationName } = useControls({
+    animationName: { options: animations.names },
+  });
 
   useEffect(() => {
-    const action = animations.actions.Run;
-    action.play();
-  },[]);
+    const action = animations.actions[animationName];
+    action.reset().fadeIn(0.5).play();
+
+    return () => action.fadeOut(0.5);
+  }, [animationName]);
 
   return (
     <primitive
