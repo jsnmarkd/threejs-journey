9955dd62caef9516d1ae716ad16f15978b4702fa
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-05 16:56:53 -0400

    Import the portal scene

--- 07-react-three-fiber/49-portal-scene-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/49-portal-scene-with-r3f/src/Experience.jsx
@@ -1,14 +1,20 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF, useTexture, Center } from "@react-three/drei";
 
 export default function Experience() {
+  const { nodes } = useGLTF("./model/portal.glb");
+  const bakedTexture = useTexture("./model/baked.jpg");
+  bakedTexture.flipY = false;
+
   return (
     <>
+      <color args={["#030202"]} attach={"background"} />
       <OrbitControls makeDefault />
 
-      <mesh scale={1.5}>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} />
+        </mesh>
+      </Center>
     </>
   );
 }
