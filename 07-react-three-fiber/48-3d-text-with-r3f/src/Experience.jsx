691d027cf0606b8ef1a78a3430253d65f6983048
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-05 14:19:53 -0400

    Optimization - useState, method 1

--- 07-react-three-fiber/48-3d-text-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/48-3d-text-with-r3f/src/Experience.jsx
@@ -5,8 +5,12 @@ import {
   useMatcapTexture,
 } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import { useState } from "react";
 
 export default function Experience() {
+  const [torusGeometry, setTorusGeometry] = useState();
+  const [material, setMaterial] = useState();
+
   const [matcapTexture] = useMatcapTexture("85B9D3_C9EAF9_417277_528789", 256);
 
   return (
@@ -15,8 +19,12 @@ export default function Experience() {
 
       <OrbitControls makeDefault />
 
+      <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+      <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
+
       <Center>
         <Text3D
+          material={material}
           font={"./fonts/helvetiker_regular.typeface.json"}
           size={0.75}
           height={0.2}
@@ -28,13 +36,14 @@ export default function Experience() {
           bevelSegments={5}
         >
           JASON
-          <meshMatcapMaterial matcap={matcapTexture} />
         </Text3D>
       </Center>
 
       {[...Array(100)].map((_, index) => (
         <mesh
           key={index}
+          geometry={torusGeometry}
+          material={material}
           position={[
             (Math.random() - 0.5) * 10,
             (Math.random() - 0.5) * 10,
@@ -42,10 +51,7 @@ export default function Experience() {
           ]}
           scale={0.2 + Math.random() * 0.2}
           rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-        >
-          <torusGeometry args={[1, 0.6, 16, 32]} />
-          <meshMatcapMaterial matcap={matcapTexture} />
-        </mesh>
+        />
       ))}
     </>
   );
