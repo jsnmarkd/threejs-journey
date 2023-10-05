38a30f49affb5c9bfdd3cb6817738f0fc4b9be01
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-05 17:15:05 -0400

    Import the pole lights and portal, and adjust the tone mapping

--- 07-react-three-fiber/49-portal-scene-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/49-portal-scene-with-r3f/src/Experience.jsx
@@ -14,6 +14,26 @@ export default function Experience() {
         <mesh geometry={nodes.baked.geometry}>
           <meshBasicMaterial map={bakedTexture} />
         </mesh>
+
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+        >
+          <meshBasicMaterial color={"#ffffe5"} />
+        </mesh>
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+        >
+          <meshBasicMaterial color={"#ffffe5"} />
+        </mesh>
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        >
+          <meshBasicMaterial color={"#ffffff"} />
+        </mesh>
       </Center>
     </>
   );
