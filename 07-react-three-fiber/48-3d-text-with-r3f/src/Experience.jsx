426e6f60bedde54807a290a52e21bb19736d078c
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-05 13:40:32 -0400

    Add the 3D text with Drei helper function, Center the text, add some parameters and use a Matcap Texture

--- 07-react-three-fiber/48-3d-text-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/48-3d-text-with-r3f/src/Experience.jsx
@@ -1,17 +1,35 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  OrbitControls,
+  Text3D,
+  Center,
+  useMatcapTexture,
+} from "@react-three/drei";
 import { Perf } from "r3f-perf";
 
 export default function Experience() {
+  const [matcapTexture] = useMatcapTexture("85B9D3_C9EAF9_417277_528789", 256);
   return (
     <>
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
-      <mesh scale={1.5}>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Center>
+        <Text3D
+          font={"./fonts/helvetiker_regular.typeface.json"}
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          JASON
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </Text3D>
+      </Center>
     </>
   );
 }
