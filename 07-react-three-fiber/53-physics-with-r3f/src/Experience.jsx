f0bcebdff703d895b5c6c3ae9e5821d64d121baf
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-01 17:39:05 -0400

    Implementing Ball and Hull colliders

--- 07-react-three-fiber/53-physics-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/53-physics-with-r3f/src/Experience.jsx
@@ -12,18 +12,28 @@ export default function Experience() {
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <Physics>
-        <RigidBody>
-          <mesh castShadow position={[-2, 2, 0]}>
+      <Physics debug>
+        {/* Sphere */}
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[0, 4, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
 
-        <mesh castShadow position={[2, 2, 0]}>
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        {/* Torus */}
+        <RigidBody colliders="hull">
+          <mesh
+            castShadow
+            position={[0, 1, -0.25]}
+            rotation={[Math.PI * 0.1, 0, 0]}
+          >
+            <torusGeometry args={[1, 0.5, 16, 32]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody>
+
+        {/* Plane */}
         <RigidBody type="fixed">
           <mesh receiveShadow position-y={-1.25}>
             <boxGeometry args={[10, 0.5, 10]} />
