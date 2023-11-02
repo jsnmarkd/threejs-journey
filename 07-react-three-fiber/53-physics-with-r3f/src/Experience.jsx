81de0dc73e4bda1fbcbec0e774c58c21ed940609
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-02 16:47:41 -0400

    Apply forces to the cube mesh on click

--- 07-react-three-fiber/53-physics-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/53-physics-with-r3f/src/Experience.jsx
@@ -1,13 +1,19 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import {
-  Physics,
-  RigidBody,
-  CuboidCollider,
-  BallCollider,
-} from "@react-three/rapier";
+import { Physics, RigidBody } from "@react-three/rapier";
+import { useRef } from "react";
 
 export default function Experience() {
+  const cube = useRef();
+  const cubeJump = () => {
+    cube.current.applyImpulse({ x: 0, y: 5, z: 0 });
+    cube.current.applyTorqueImpulse({
+      x: Math.random() - 0.5,
+      y: Math.random() - 0.5,
+      z: Math.random() - 0.5,
+    });
+  };
+
   return (
     <>
       <Perf position="top-left" />
@@ -20,22 +26,15 @@ export default function Experience() {
       <Physics debug>
         {/* Sphere */}
         <RigidBody colliders="ball">
-          <mesh castShadow position={[0, 4, 0]}>
+          <mesh castShadow position={[-1.5, 2, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
 
-        {/* Torus */}
-        <RigidBody
-          colliders={false}
-          position={[0, 1, 0]}
-          rotation={[Math.PI * 0.5, 0, 0]}
-        >
-          <BallCollider args={[1.5]} />
-
-          <mesh castShadow>
-            <torusGeometry args={[1, 0.5, 16, 32]} />
+        <RigidBody ref={cube} position={[1.5, 2, 0]}>
+          <mesh castShadow onClick={cubeJump}>
+            <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
         </RigidBody>
