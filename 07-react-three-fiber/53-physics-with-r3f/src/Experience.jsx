efba24fbbbcea0ffe34db3c1adfe8891c99feb60
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-06 10:14:11 -0500

    Add mass to the cube mesh

--- 07-react-three-fiber/53-physics-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/53-physics-with-r3f/src/Experience.jsx
@@ -1,12 +1,13 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { Physics, RigidBody } from "@react-three/rapier";
+import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
 import { useRef } from "react";
 
 export default function Experience() {
   const cube = useRef();
   const cubeJump = () => {
-    cube.current.applyImpulse({ x: 0, y: 5, z: 0 });
+    const mass = cube.current.mass();
+    cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 });
     cube.current.applyTorqueImpulse({
       x: Math.random() - 0.5,
       y: Math.random() - 0.5,
@@ -39,7 +40,9 @@ export default function Experience() {
           gravityScale={1}
           restitution={0}
           friction={0.7}
+          colliders={false}
         >
+          <CuboidCollider mass={2} args={[0.5, 0.5, 0.5]} />
           <mesh castShadow onClick={cubeJump}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
