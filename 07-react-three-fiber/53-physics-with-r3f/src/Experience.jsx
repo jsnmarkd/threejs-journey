b67185105da71d4a706af1f02a41125a9dcbd018
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-07 10:42:18 -0500

    Stress test - add invisble walls so that objects do not fall off the scene using cuboid colliders

--- 07-react-three-fiber/53-physics-with-r3f/src/Experience.jsx
+++ 07-react-three-fiber/53-physics-with-r3f/src/Experience.jsx
@@ -110,6 +110,14 @@ export default function Experience() {
           <CylinderCollider args={[0.5, 1.25]} />
           <primitive object={hamburger.scene} scale={0.25} />
         </RigidBody>
+
+        {/* Invisible Walls */}
+        <RigidBody type="fixed">
+          <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, 5.5]} />
+          <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, -5.5]} />
+          <CuboidCollider args={[0.5, 2, 5]} position={[5.25, 1, 0]} />
+          <CuboidCollider args={[0.5, 2, 5]} position={[-5.25, 1, 0]} />
+        </RigidBody>
       </Physics>
     </>
   );
